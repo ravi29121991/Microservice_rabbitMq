2f119b91ff1f3eacaa91d2691ba5952a0c2dcbc2
ravi <ravi@binmile.com>
2025-07-16 15:23:38 +0530

    product changes

--- product-service/src/health.js
+++ product-service/src/health.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
+
 router.get('/', (_, res) => res.send('Product Service is healthy'));
 module.exports = { healthRouter: router };
