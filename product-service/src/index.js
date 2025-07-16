7f2e0545732372ea86c2b503381d425f78532cc8
ravi <ravi@binmile.com>
2025-07-16 14:53:48 +0530

    psuhing to uat

--- product-service/src/index.js
+++ product-service/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db')();
 const { healthRouter } = require('./health');
 
+
 const app = express();
 app.use('/health', healthRouter);
 app.listen(3003, () => console.log('[Product Service] Running on port 3003'));
