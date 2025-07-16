1b8a955cc3e5a82d4eae7644fa2b92e75dedb7a3
ravi <ravi@binmile.com>
2025-07-16 17:55:06 +0530

    testfile

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -13,7 +13,6 @@ app.use('/health', healthRouter);
 // Axios with retry
 axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
-
 connectMongo();
 
 app.post('/users', async (req, res) => {
